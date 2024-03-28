bf697846d80817dddfc1325c4864de7a901a9f14
mahiranand <mahiranand08@gmail.com>
2024-03-28 13:31:41 +0530

    REWARD_CREATED

--- index.py
+++ index.py
@@ -26,14 +26,10 @@ def store_data_into_clickhouse(client, data):
         else:
             raise ValueError("DataFrame does not contain 'timestamp' column")
         
-        if key == "MULTISTEP_ACTIVITY_COMPLETED":
-            df['campaign_details_campaign_expired'] = df['campaign_details_campaign_expired'].astype(int, errors='ignore').fillna(0)
-            df['campaign_details_campaign_steps_completed'] = df['campaign_details_campaign_steps_completed'].astype(int, errors='ignore').fillna(0)
-            df['campaign_details_campaign_total_steps'] = df['campaign_details_campaign_total_steps'].astype(int, errors='ignore').fillna(0)
-            df['campaign_details_campaign_activity_activity_completed_total'] = df['campaign_details_campaign_activity_activity_completed_total'].astype(int, errors='ignore').fillna(0)
-            df['campaign_details_campaign_activity_activity_completed_daily'] = df['campaign_details_campaign_activity_activity_completed_daily'].astype(int, errors='ignore').fillna(0)
-            df['campaign_details_campaign_activity_activity_limits_total'] = df['campaign_details_campaign_activity_activity_limits_total'].astype(int, errors='ignore').fillna(0)
-            df['campaign_details_campaign_activity_activity_limits_daily'] = df['campaign_details_campaign_activity_activity_limits_daily'].astype(int, errors='ignore').fillna(0)
+        if key == "REWARD_CREATED":
+            df['reward_details_reward_index'] = df['reward_details_reward_index'].astype(int, errors='ignore').fillna(0)
+            df['reward_details_reward_amount'] = df['reward_details_reward_amount'].astype(float, errors='ignore').fillna(0)
+            df['reward_details_stepsCompleted'] = df['reward_details_stepsCompleted'].astype(int, errors='ignore').fillna(0)
    
         
         client.insert_dataframe(f'INSERT INTO {key} VALUES', df, settings=dict(use_numpy=True))
@@ -55,8 +51,8 @@ def makeSchema(json_data):
             one_data = flatten(one_data)
             new_data = {}
             
-            if(key == "MULTISTEP_ACTIVITY_COMPLETED"):
-                rkeys = ['type', 'event_id', 'client', 'user_id', 'analytics_version', 'timestamp', 'campaign_details_campaign_id', 'campaign_details_campaign_name', 'campaign_details_campaign_experience', 'campaign_details_campaign_status', 'campaign_details_campaign_steps_completed', 'campaign_details_campaign_total_steps', 'campaign_details_campaign_expires_on', 'campaign_details_campaign_expiry_type', 'campaign_details_campaign_expired', 'campaign_details_campaign_activity_activity_completed_total', 'campaign_details_campaign_activity_activity_completed_daily', 'campaign_details_campaign_activity_activity_limits_total', 'campaign_details_campaign_activity_activity_limits_daily', 'campaign_details_campaign_activity_campaign_activity_status', 'campaign_details_campaign_activity_campaign_activity_id', 'campaign_details_campaign_activity_campaign_activity_event_name', 'campaign_details_campaign_activity_campaign_activity_completed_on', 'event_name']
+            if(key == "REWARD_CREATED"):
+                rkeys = ["type", "event_id", "client", "user_id", "analytics_version", "timestamp", "campaign_details_campaign_id", "campaign_details_campaign_name", "campaign_details_campaign_experience", "campaign_details_campaign_status", "reward_details_event_name", "reward_details_brand_name", "reward_details_series_id", "reward_details_stepsCompleted", "reward_details_activityId", "reward_details_reward_coupon_code", "reward_details_reward_index", "reward_details_reward_id", "reward_details_reward_status", "reward_details_reward_title", "reward_details_reward_body", "reward_details_reward_type", "reward_details_reward_amount", "reward_details_audiance_id", "reward_details_key", "event_name"]
 
                 for rkey in rkeys:
                     if one_data.get(rkey) is not None:
@@ -112,7 +108,7 @@ if __name__ == '__main__':
     # json_data = read_json_data_from_azure(blob_client, AZURE_CONTAINER, 'fe-page/2024-02-14/00')
     # filteredData = makeSchema(json_data)
     # store_data_into_clickhouse(client, filteredData)
-    jan_path = 'multistep-activity-completed/2024-01-'
+    jan_path = 'reward-created/2024-01-'
 
     for i in range(1, 32):
         i = "{:02d}".format(i)
@@ -128,7 +124,7 @@ if __name__ == '__main__':
             filtered_data = makeSchema(json_data)
             store_data_into_clickhouse(client, filtered_data)
 
-    feb_path = 'multistep-activity-completed/2024-02-'
+    feb_path = 'reward-created/2024-02-'
 
     for i in range(1, 30):
         i = "{:02d}".format(i)
@@ -144,7 +140,7 @@ if __name__ == '__main__':
             filtered_data = makeSchema(json_data)
             store_data_into_clickhouse(client, filtered_data)
     
-    mar_path = 'multistep-activity-completed/2024-03-'
+    mar_path = 'reward-created/2024-03-'
     
     for i in range(1, 15):
         i = "{:02d}".format(i)
